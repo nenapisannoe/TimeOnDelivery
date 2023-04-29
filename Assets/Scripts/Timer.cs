eb810a7571fcfcddbfdda99a879b9c85652df577
i am nena <58001221+nenapisannoe@users.noreply.github.com>
2023-04-29 17:34:51 +0300

    timer implemented

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -1,22 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.UI;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Timer : MonoBehaviour
 {
     [SerializeField] float timeLeft = 30.0f;
-     
+    [SerializeField] private Text timerText;
+    private bool timeOver = false;
+
+    void Start()
+    {
+        timeOver = false;
+    }
     void Update()
     {
-        timeLeft -= Time.deltaTime;
-        if(timeLeft < 0)
+        if (!timeOver)
         {
-            GameOver();
+            timeLeft -= Time.deltaTime;
+            DisplayTime(timeLeft);
+            if (timeLeft <= 0)
+            {
+                timeOver = true;
+                GameOver();
+            }
         }
+    }
+    
+    void DisplayTime(float timeToDisplay)
+    {
+        timeToDisplay += 1;
+        float minutes = Mathf.FloorToInt(timeToDisplay / 60); 
+        float seconds = Mathf.FloorToInt(timeToDisplay % 60);
+        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
     }
 
     void GameOver()
     {
-        
+        SceneManager.LoadScene("GameOver");
     }
 }
