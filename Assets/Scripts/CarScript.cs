b7601f439a67465fad9fdd811f1c3669ba3612c5
caisilus <serangor@gmail.com>
2023-04-29 16:59:12 +0300

    Add crosswalk

--- Assets/Scripts/CarScript.cs
+++ Assets/Scripts/CarScript.cs
@@ -8,10 +8,63 @@ public class CarScript : MonoBehaviour
 
     private bool _shouldMove = false;
 
+    public bool Stoped { get; private set; } = false;
+
     void Update()
     {
-        if (!_shouldMove) return;
+        if (!_shouldMove)
+        {
+            MoveToCrossWalk();
+        }
+        else
+        {
+            RegularMove();
+        }
+    }
 
+    void MoveToCrossWalk()
+    {
+        RaycastHit hit;
+        Physics.Raycast(transform.position, transform.right, out hit);
+
+        Vector3 movement = transform.right * _speed * Time.deltaTime;
+
+        Stoped = ShouldStop(hit, movement);
+        
+        if (!Stoped)
+        {
+            transform.Translate(movement, Space.World);
+        }
+    }
+
+    bool ShouldStop(RaycastHit hit, Vector3 movement)
+    {
+        var hitObjectTag = hit.collider.tag;
+        
+        if (hitObjectTag == "CrossWalk")
+        {
+            var crossWalkWidth = 3;
+            return hit.distance < movement.magnitude + crossWalkWidth;
+        }
+
+        if (hitObjectTag == "Car")
+        {
+            CarScript nextCar = hit.collider.gameObject.GetComponent<CarScript>();
+            if (nextCar == null)
+            {
+                return true;
+            }
+
+            return nextCar.Stoped && (hit.distance < movement.magnitude);
+        }
+
+        return false;
+    }
+
+    void RegularMove()
+    {
+        Stoped = false;
+        
         // Right just because models are imported the way car's forward is red axis
         Vector3 movement = transform.right * _speed * Time.deltaTime;
 
