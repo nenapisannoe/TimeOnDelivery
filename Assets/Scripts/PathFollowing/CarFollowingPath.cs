84bec67ead45fd7d506fa3b25f2165bfdb3cdf75
caisilus <serangor@gmail.com>
2023-04-30 14:07:25 +0300

    Refactoring

--- Assets/Scripts/PathFollowing/CarFollowingPath.cs
+++ Assets/Scripts/PathFollowing/CarFollowingPath.cs
@@ -16,13 +16,8 @@ public class CarFollowingPath : MonoBehaviour
 
     private const float EPS = 0.001f;
 
-    void Start()
-    {
-        _currentPathIndex = 0;
-        _t = 0;
-    }
-
-    private void Update()
+    // Not pure
+    protected Vector3 NextPosition()
     {
         if (Mathf.Abs(_t - 1.0f) < EPS)
         {
@@ -32,7 +27,7 @@ public class CarFollowingPath : MonoBehaviour
 
         if (_currentPathIndex >= _paths.Length)
         {
-            if (!_isLooped) return;
+            if (!_isLooped) return transform.position;
 
             _currentPathIndex = 0;
         }
@@ -41,12 +36,42 @@ public class CarFollowingPath : MonoBehaviour
 
         Vector3 newPosition = path.GetPathPoint(_t);
 
-        Vector3 movement = newPosition - transform.position;
-        transform.position = newPosition;
+        return newPosition;
+    }
 
-        var newRotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), _rotationSpeed);
+    // Pure
+    protected Quaternion NextRotation(Vector3 nextPosition)
+    {
+        Vector3 movement = nextPosition - transform.position;
+
+        if (movement.magnitude < EPS)
+        {
+            return Quaternion.identity;
+        }
+
+        return Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), _rotationSpeed);
+    }
+
+    protected void Move()
+    {
+        var newPosition = NextPosition();
+        var newRotation = NextRotation(newPosition);
+
+        // Has to be later than NextRotation because it uses previous position refering transform.position
+        transform.position = newPosition;
         transform.rotation = newRotation;
 
         _t += Time.deltaTime * _speed;
+    }
+
+    private void Start()
+    {
+        _currentPathIndex = 0;
+        _t = 0;
+    }
+
+    private void Update()
+    {
+        Move();
     }
 }
