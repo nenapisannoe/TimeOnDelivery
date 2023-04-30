0953d02fe46f3ea8d57ec663187377d0c0bbbd50
caisilus <serangor@gmail.com>
2023-04-30 19:22:33 +0300

    Tiny refactoring

--- Assets/Scripts/DeliveryManager.cs
+++ Assets/Scripts/DeliveryManager.cs
@@ -4,21 +4,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class PickUpCargo : MonoBehaviour
+public class DeliveryManager : MonoBehaviour
 {
     [SerializeField] private GameObject bag;
     [SerializeField] public bool cargoPicked;
 
-    void Start()
+    private void Start()
     {
         cargoPicked = false;
     }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Store"))
         {
             bag.GetComponent<Renderer>().material.color = Color.red;
             cargoPicked = true;
+            return;
         }
 
         if (other.CompareTag("Finish"))
