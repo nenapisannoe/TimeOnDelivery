f1df9d3bc633f266fca2bf130dcaf6babcf2cadd
caisilus <serangor@gmail.com>
2023-04-30 16:24:49 +0300

    Character gravity

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,7 +7,9 @@ public class PlayerController: MonoBehaviour
 {
     [SerializeField] private float _speed = 8.0f;
     [SerializeField] private float _rotationSpeed = 0.15f;
-    [SerializeField] private CharacterController _characterController;
+    [SerializeField] private float _gravity = -9.8f;
+    
+    private CharacterController _characterController;
 
     private Vector2 _move;
 
@@ -34,6 +36,8 @@ public class PlayerController: MonoBehaviour
             return;
 
         transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), _rotationSpeed);
+
+        movement.y = _gravity;
 
         _characterController.Move(movement * _speed * Time.deltaTime);
         // transform.Translate(movement * _speed * Time.deltaTime, Space.World);
