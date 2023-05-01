e4a0f184980248f4bd81cb2512f092584ad0e807
i am nena <58001221+nenapisannoe@users.noreply.github.com>
2023-05-01 23:07:24 +0300

    tutorial updated

--- Assets/Scripts/Tutorial2Scenario.cs
+++ Assets/Scripts/Tutorial2Scenario.cs
@@ -8,15 +8,19 @@ public class Tutorial2Scenario : MonoBehaviour
 {
     [SerializeField] private DeliveryManager _player;
     [SerializeField] private Boots _boots;
+    [SerializeField] private Ghost _ghost;
     [SerializeField] private Text TutorialText;
+    [SerializeField] private GameObject powerup; 
     [SerializeField] private bool step2done;
     [SerializeField] private bool step3done;
     [SerializeField] private bool step4done;
     [SerializeField] private bool step5done;
+    [SerializeField] private bool step6done;
     void Start()
     {
         _player = FindObjectOfType<DeliveryManager>();
         _boots = FindObjectOfType<Boots>(true);
+        _ghost = FindObjectOfType<Ghost>(true);
         TutorialText.text = "New delivery task! First, pick up the package!";
         
     }
@@ -25,8 +29,7 @@ public class Tutorial2Scenario : MonoBehaviour
     {
         if (!step2done && _player.cargoPicked )
         {
-            var powerup = FindObjectOfType<PowerUp>(true);
-            powerup.gameObject.SetActive(true);
+            powerup.SetActive(true);
             StepTwo();
             step2done = true;
         }
@@ -43,15 +46,19 @@ public class Tutorial2Scenario : MonoBehaviour
             step4done = true;
         }
 
-        if (step4done && _player.deliveredInTutorial)
+        if (step4done && _ghost.enabled)
         {
-            TutorialText.text = "Excellent! The package has been delivered! Time for your next task. If you want to start the level again, press 'R', and if you are ready to get to work, press Space. Good luck! ";
+            StepFive();
             step5done = true;
         }
-        
+        if (step5done && _player.deliveredInTutorial)
+        {
+            TutorialText.text = "Excellent! The package has been delivered! Time for your next task. If you want to start the level again, press 'R', and if you are ready to get to work, press Space. Good luck! ";
+            step6done = true;
+        }
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            if (step5done)
+            if (step6done)
             {
                 SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
             }
@@ -71,5 +78,10 @@ public class Tutorial2Scenario : MonoBehaviour
     private void StepTwo()
     {
         TutorialText.text = "Look! Our noble corporation left something for you to improve your work performance! Why dont you go and pick it up."; 
+    }
+
+    private void StepFive()
+    {
+        TutorialText.text = "Look, another power up! This one can help you with those obstacles that slow you down. With these on, you dont have to worry about them.";
     }
 }
